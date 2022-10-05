df86da04528e196618662fc8aac6d52787942982
okamotokeita <okamoto.keita1937@gmail.com>
2022-10-06 02:20:26 +0900

    abc101自動テスト化

--- Main.java
+++
@@ -1,7 +0,0 @@
-package atcoder;
-
-class Main {
-    public static void main(String [] args) {
-      System.out.println("Hello Java!!!!!!");
-    }
-  }
---
+++ abc101/a/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+      final String plus = "+";
+      try(Scanner scan = new Scanner(System.in)){
+        int total = 0;
+        
+        String line = scan.nextLine();
+        String[] marks = line.split("");
+        for(String mark : marks){
+          total = total + (mark.equals(plus)  ? 1 : -1);
+        }         
+        System.out.println(total);
+      }
+    }
+}
