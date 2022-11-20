7800dd7ec2f4e7b9251acf0e217e3174b1e30f30
okamotokeita <okamoto.keita1937@gmail.com>
2022-11-20 17:52:18 +0900

    abc278 c 修正

--- abc278/c/Main.java
+++ abc278/c/Main.java
@@ -1,6 +1,6 @@
-
-
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
@@ -9,30 +9,22 @@ public class Main {
          
           int N = Integer.parseInt(scan.next());
           int Q = Integer.parseInt(scan.next());
-          boolean [][] glf = new boolean[N+1][N+1];
-          int T;
-          int A;
-          int B;
+          Set<String> set = new HashSet<String>();
+          int T,A,B;
           for(int i = 0; i < Q; i++){
              T = Integer.parseInt(scan.next());
              A = Integer.parseInt(scan.next());
              B = Integer.parseInt(scan.next());
              if(T == 1){
-              follow(A,B,glf);
+              set.add(A + " " + B);
              }
              if(T == 2){
-              followOut(A,B,glf);
+              set.remove(A + " " + B);
              }
              if(T == 3){
-              System.out.println(glf[A][B]&&glf[B][A] ? "Yes" : "No");
+              System.out.println(set.contains(A + " " + B)&&set.contains(B + " " + A) ? "Yes" : "No");
              }
           }
         }
-    }
-    static void follow(int follower,int followed,boolean[][] glf){
-      glf[follower][followed] = true;
-    }
-    static void followOut(int follower,int followed,boolean[][] glf){
-      glf[follower][followed] = false;
     }
 }
